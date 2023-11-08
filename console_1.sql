7d56b331034600b218bbe45e58448ad01c1e7b58
joachim-qi-vinci <joachim.qi@student.vinci.be>
2023-11-08 13:20:16 +0100

    initial commit

---
+++ console_1.sql
@@ -0,0 +1,9 @@
+DROP SCHEMA IF EXISTS projet CASCADE;
+CREATE SCHEMA projet;
+
+CREATE TABLE projet.etudiants (
+    matricule_etudiant SERIAL PRIMARY KEYo
+
+                              helloooo
+
+);
