d8fa785c584b563bd6b23d22b05ebae847bdf4ea
joachim-qi-vinci <joachim.qi@student.vinci.be>
2023-11-14 00:51:29 +0100

    added app professeur p. 6, 7 AND INSERT INTO etudiant, candidatures

--- projetbdrequetes2.sql
+++ projetbdrequetes2.sql
@@ -15,11 +15,11 @@ CREATE TABLE projet.etudiants
     nom VARCHAR(40) NOT NULL,
     prenom VARCHAR(40) NOT NULL,
     mail VARCHAR(50) NOT NULL
-        CHECK ( mail SIMILAR TO '[a-z].[a-z]@student.vinci.be'),
+        CHECK (mail SIMILAR TO '[a-z]+\.[a-z]+@student\.vinci\.be'),
     semestre_stage semestre_de_stage NOT NULL
         CHECK (etudiants.semestre_stage IN ('Q1', 'Q2')),
     mdp VARCHAR(20) NOT NULL,
-    nbr_candidatures_en_attente INTEGER NOT NULL
+    nbr_candidatures_en_attente INTEGER NOT NULL DEFAULT 0
 );
 
 CREATE TABLE projet.entreprises
@@ -56,7 +56,7 @@ CREATE TABLE projet.candidatures
     etudiant    INTEGER REFERENCES projet.etudiants (matricule_etudiant) NOT NULL,
     offre_stage INTEGER REFERENCES projet.offres_stage(id_offre_stage) NOT NULL,
     motivation VARCHAR(200) NOT NULL,
-    etat etat_candidature NOT NULL,
+    etat etat_candidature NOT NULL DEFAULT 'en attente',
     PRIMARY KEY (etudiant, offre_stage)
 );
 
@@ -88,13 +88,23 @@ INSERT INTO projet.offres_stage(entreprise, code_offre_stage, description, semes
 INSERT INTO projet.offres_stage(entreprise, code_offre_stage, description, semestre_offre) VALUES ('HUA','HUA1','Gros stage de haut niveau chez les chinois','Q1');
 INSERT INTO projet.offres_stage(entreprise, code_offre_stage, description, semestre_offre) VALUES ('SAM', 'SAM1', 'Petit stage sympathique chez Samsung', 'Q2');
 
+--INSERT INTO ETUDIANTS
+INSERT INTO projet.etudiants(nom, prenom, mail, semestre_stage, mdp) VALUES ('Qi', 'Joachim', 'joachim.qi@student.vinci.be', 'Q1', '1234');
+INSERT INTO projet.etudiants(nom, prenom, mail, semestre_stage, mdp) VALUES ('Margjini', 'Mario', 'mario.margjini@student.vinci.be', 'Q1', '1234');
+INSERT INTO projet.etudiants(nom, prenom, mail, semestre_stage, mdp) VALUES ('Salle', 'Robin', 'robin.salle@student.vinci.be', 'Q2', '1234');
+
+--INSERT INTO CANDIDATURES
+INSERT INTO projet.candidatures(etudiant, offre_stage, motivation) VALUES (1, 4, 'Chinese Gang');
+INSERT INTO projet.candidatures(etudiant, offre_stage, motivation) VALUES (3, 3, '420Bedave');
+INSERT INTO projet.candidatures(etudiant, offre_stage, motivation) VALUES (2, 2, 'Albanian Mafia');
+
 
 -- L’encodage échouera si le mot clé est déjà présent
 CREATE OR REPLACE FUNCTION projet.trigger() RETURNS TRIGGER AS $$
 BEGIN
     -- Vérifier si le mot-clé existe déjà
     IF EXISTS(SELECT * FROM projet.mots_cles mc
-               WHERE mc.intitule = NEW.intitule) THEN
+              WHERE mc.intitule = NEW.intitule) THEN
         -- Lever une exception si le mot-clé existe déjà
         RAISE EXCEPTION 'Ce mot-clé existe déjà';
     END IF;
@@ -109,12 +119,27 @@ CREATE TRIGGER trigger_mot_cle BEFORE INSERT ON projet.mots_cles
 
 
 
--- Voir les offres de stage dans l’état « non validée ». Pour chaque offre, on affichera son
--- code, son semestre, le nom de l’entreprise et sa description
+-- APP PROFESSEUR 4.
 
 SELECT os.id_offre_stage, os.code_offre_stage AS code_de_stage, os.semestre_offre AS semestre, e.nom AS entreprise, os.description
 FROM projet.offres_stage os, projet.entreprises e
 WHERE os.entreprise = e.id_entreprise AND os.etat = 'non-validée'
 ORDER BY semestre_offre, e.id_entreprise;
 
+--APP PROFESSEUR 6.
+--Voir les offres de stage dans l’état « validée ». Même affichage qu’au point 4.
 
+SELECT os.id_offre_stage, os.code_offre_stage AS code_de_stage, os.semestre_offre AS semestre, e.nom AS entreprise, os.description
+FROM projet.offres_stage os, projet.entreprises e
+WHERE os.entreprise = e.id_entreprise AND os.etat = 'validée'
+ORDER BY semestre_offre, e.id_entreprise;
+
+--APP PROFESSEUR 7.
+--Voir les étudiants qui n’ont pas de stage (pas de candidature à l’état « acceptée »).
+--Pour chaque étudiant, on affichera son nom, son prénom, son email, le semestre où il
+--fera son stage et le nombre de ses candidatures en attente.
+
+SELECT et.nom, et.prenom, et.mail, et.semestre_stage, COUNT(c.*)
+FROM projet.etudiants et, projet.candidatures c
+WHERE et.matricule_etudiant = c.etudiant AND c.etat != 'acceptée'
+GROUP BY et.nom, et.prenom, et.mail, et.semestre_stage;
