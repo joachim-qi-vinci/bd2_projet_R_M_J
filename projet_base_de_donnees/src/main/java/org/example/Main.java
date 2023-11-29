1efdcf7f8658920c96d177e752f61807322e187d
Qi Joachim <129367758+joachim-qi-vinci@users.noreply.github.com>
2023-11-29 16:44:51 +0100

    Add files via upload

---
+++ projet_base_de_donnees/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        GestionStage gestionStage = new GestionStage();
+        gestionStage.run();
+    }
+}
